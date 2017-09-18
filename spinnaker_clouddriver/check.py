4f11c67ec1dd568cda129c854c4530f2a2b995a6
Seigo Uchida <spesnova@gmail.com>
2017-09-18 11:43:59 +0900

    Add health check for spinnaker-clouddriver

--- spinnaker_clouddriver/check.py
+++ spinnaker_clouddriver/check.py
@@ -5,14 +5,57 @@
 # stdlib
 
 # 3rd party
+import requests
 
 # project
 from checks import AgentCheck
 
-EVENT_TYPE = SOURCE_TYPE_NAME = 'spinnaker_clouddriver'
-
-
 class Spinnaker_clouddriverCheck(AgentCheck):
+    DEFAULT_TIMEOUT = 5
+    SERVICE_CHECK_NAME = 'spinnaker.clouddriver.health'
 
     def check(self, instance):
-        pass
+        url = instance['url']
+        timeout = float(instance.get('timeout', self.DEFAULT_TIMEOUT))
+
+        health = self._get_health(url, timeout)
+        if health is not None:
+            self.service_check(
+                self.SERVICE_CHECK_NAME,
+                AgentCheck.OK,
+                tags=["url:{0}".format(url)],
+            )
+
+    def _get_health(self, url, timeout):
+        return self._get_json(url + '/health', timeout)
+
+    def _get_json(self, url, timeout):
+        try:
+            r = requests.get(url, timeout=timeout)
+        except requests.exceptions.Timeout:
+            self.service_check(
+                self.SERVICE_CHECK_NAME,
+                AgentCheck.CRITICAL,
+                message="Timeout when hitting %s" % url,
+                tags=["url:{0}".format(url)],
+            )
+            raise
+        except Exception as e:
+            self.service_check(
+                self.SERVICE_CHECK_NAME,
+                AgentCheck.CRITICAL,
+                message="Error hitting %s. Error: %s" % (url, e.message),
+                tags=["url:{0}".format(url)],
+            )
+            raise
+
+        if r.status_code != 200:
+            self.service_check(
+                self.SERVICE_CHECK_NAME,
+                AgentCheck.CRITICAL,
+                message="Got %s when hitting %s" % (r.status_code, url),
+                tags=["url:{0}".format(url)]
+            )
+            raise Exception("Http status code {0} on url {1}".format(r.status_code, url))
+
+        return r.json()
--- spinnaker_clouddriver/test_spinnaker_clouddriver.py
+++ spinnaker_clouddriver/test_spinnaker_clouddriver.py
@@ -6,33 +6,57 @@
 from nose.plugins.attrib import attr
 
 # 3p
+import mock
+import json
 
 # project
 from tests.checks.common import AgentCheckTest
 
-
-instance = {
-    'host': 'localhost',
-    'port': 26379,
-    'password': 'datadog-is-devops-best-friend'
+MOCK_OK_CONFIG = {
+    'init_config': {},
+    'instances': [
+        {'url': 'http://localhost:7002'}
+    ]
+}
+MOCK_CRITICAL_CONFIG = {
+    'init_config': {},
+    'instances': [
+        {'url': 'http://badurl:7002'}
+    ]
 }
 
+class MockResponse:
+   def __init__(self, json_data, status_code):
+       self.json_data = json_data
+       self.status_code = status_code
 
-# NOTE: Feel free to declare multiple test classes if needed
+   def json(self):
+       return self.json_data
 
-@attr(requires='spinnaker_clouddriver')
+def mock_requests_get(*args, **kwargs):
+
+    if args[0] == 'http://localhost:7002/health':
+        return MockResponse({"status": "UP"}, 200)
+    else:
+        return MockResponse(None, 404)
+
 class TestSpinnaker_clouddriver(AgentCheckTest):
-    """Basic Test for spinnaker_clouddriver integration."""
     CHECK_NAME = 'spinnaker_clouddriver'
+    SERVICE_CHECK_NAME = 'spinnaker.clouddriver.health'
 
-    def test_check(self):
-        """
-        Testing Spinnaker_clouddriver check.
-        """
-        self.load_check({}, {})
+    @mock.patch('requests.get', side_effect=mock_requests_get)
+    def test_ok_service_check(self, mock_requests):
+        self.run_check(MOCK_OK_CONFIG)
+        self.print_current_state()
+        self.assertServiceCheckOK(self.SERVICE_CHECK_NAME)
+        self.coverage_report()
 
-        # run your actual tests...
-
-        self.assertTrue(True)
-        # Raises when COVERAGE=true and coverage < 100%
+    @mock.patch('requests.get', side_effect=mock_requests_get)
+    def test_critical_service_check(self, mock_requests):
+        self.assertRaises(
+            Exception,
+            lambda: self.run_check(MOCK_CRITICAL_CONFIG)
+        )
+        self.print_current_state()
+        self.assertServiceCheckCritical(self.SERVICE_CHECK_NAME)
         self.coverage_report()
